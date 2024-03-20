bb44add66cdba3ac83d901742519a946c8c64e50
alfitosantosa <44111010166@student.mercubuana.ac.id>
2024-03-21 01:29:38 +0700

    revisi awal

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
             {" "}
             <div className="mx-auto">
               <img
-                src="https://hendihermawan.files.wordpress.com/2010/07/logodepok.png"
+                src="https://pbb-bphtb.depok.go.id:58083/e_pbb/assets/img/landing_hero_01.png"
                 alt="Ikhwaan"
                 width={100}
               />
@@ -19,10 +19,18 @@ function App() {
             <Button href="https://sukmajaya.depok.go.id/home" className="">
               PORTAL RESMI KOTA DEPOK
             </Button>
-            <Button>WHATSUP DISDUKCAPIL</Button>
+            <Button href="https://silondobermula.depok.go.id/layanan/">
+              LAYANAN DISDUKCAPIL
+            </Button>
             <Button href="https://wa.link/24q18y">WHATSUP PENGADUAN</Button>
             <Button href="https://ombudsman.go.id/pengaduan">
               PENGADUAN ONLINE OMBUDSMAN RI
+            </Button>
+            <Button href="https://drive.google.com/drive/folders/1-82_akdTe03nnhRpv4e2qniivDgtR8ru">
+              PEMBUATAN AHLI WARIS
+            </Button>
+            <Button href="https://bkol.depok.go.id/register/4">
+              PEMBUATAN KARTU KUNING
             </Button>
           </Card>
         </div>
